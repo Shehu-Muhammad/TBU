3ea4d8a2fbca87182cbd6ff858cb245d2d54f9b3
Shehu Muhammad <shehumuhammad@my.ccsu.edu>
2019-07-05 20:04:27 -0400

    Added missing underscore

--- TBU/app.js
+++ TBU/app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir:_dirname + `/views/layouts/`}));
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + `/views/layouts/`}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
